c999fc564c6c2ee1cca69960394cf3a8b37e991b
Glitch (basic-training-ii) <none>
2019-06-16 16:11:04 +0000

    👄🏥 Checkpoint ./twitter_scraper_fetcher.py:2183596/334

--- twitter_scraper_fetcher.py
+++ twitter_scraper_fetcher.py
@@ -32,7 +32,16 @@ def get_user_tweets(twitter_handle):
 
     return tweets
   
-            
+def clean_tweets_data(tweets): 
+  emoji_pattern = re.compile(
+        "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "]+",
+        flags=re.UNICODE,
+    )         
                 
 
     
