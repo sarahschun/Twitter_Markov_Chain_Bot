9c7c9162b0e45a9b0460af298c6b3966db214220
Glitch (basic-training-ii) <none>
2019-06-16 15:17:04 +0000

    🍡🍰 Checkpoint ./twitter_scraper_fetcher.py:2183596/845

--- twitter_scraper_fetcher.py
+++ twitter_scraper_fetcher.py
@@ -8,13 +8,29 @@ TWITTER_URL = "https://twitter.com/"
 
 
 def get_elements(twitter_handle):
-  ## Nothing here yet!
-  pass
-    
+    # Get HTML content from twitter profile page
+    url = TWITTER_URL + twitter_handle
+    response = requests.get(url)
+    html = response.content
+
+    # handles HTML entities
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # Find tweets usign class name
+    return soup.find_all(CONTENT_CONTAINER_TAGS, attrs={"class": CONTENT_CLASS_NAME})
+
+
 def get_user_tweets(twitter_handle):
-  ## Nothing here yet!
-  pass
-    
+    elements = get_elements(twitter_handle)
+    tweets = []
+
+    for post in elements:
+        for text in post.contents:
+            # check if line contains real text
+            if text.string not in EMPTY_ITEMS:
+                tweets.append(text.string)
+
+    return tweets
   
             
                 
