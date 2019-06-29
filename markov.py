0fc155d22bedf55c5c4efc5a57cde364d4c9ce02
Glitch (basic-training-ii) <none>
2019-06-29 16:15:08 +0000

    📙🌚 Checkpoint ./markov.py:2183596/28

--- markov.py
+++ markov.py
@@ -20,12 +20,7 @@ def generate_bot_answer_with_text_model(text_model):
     pass
     ## write code here
     
-    
-    
-    
-    
-    
-    
+ 
 # build the markov chain based on the text we read
 # we use the markovify library to do this step
 def generate_bot_answer(twitter_handle, user_question):
