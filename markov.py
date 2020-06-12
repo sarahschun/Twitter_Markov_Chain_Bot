b2f63012b5326b1008e45b52357a61d8038aa185
Glitch (basic-training-ii) <none>
2020-06-12 22:16:59 +0000

    👀🚟 Checkpoint ./markov.py:12755203/866

--- markov.py
+++ markov.py
@@ -5,18 +5,12 @@ from twitter_scraper_fetcher import *
 import random
 import re
     
- 
+    
 def generate_bot_answer_with_text_model(twitter_handle, user_question, text_model):
     pass
-    # write code here
-    
+
 # build the markov chain based on the text we read
 # we use the markovify library to do this step
 def generate_bot_answer(twitter_handle, user_question):
     pass
     # write code here
-    
-
-
-    
-    
