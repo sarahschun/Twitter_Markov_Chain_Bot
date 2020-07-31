328ae1bb87c7ab756ce959188525305bc85bc9f4
Glitch (basic-training-ii) <none>
2020-07-31 15:30:32 +0000

    👕🚶 Checkpoint ./markov.py:20186336/139

--- markov.py
+++ markov.py
@@ -1,4 +1,5 @@
 # write code here!!
+import markovify
 import config
 from mlh_twitter_api import *
 from twitter_scraper_fetcher import *
@@ -7,8 +8,10 @@ import re
     
     
 def generate_bot_answer_with_text_model(twitter_handle, user_question, text_model):
-    pass
+    bot_answer = None
 
+    word_list = user_question.split("")
+    random_word = random.choice(word_list)
 # build the markov chain based on the text we read
 # we use the markovify library to do this step
 def generate_bot_answer(twitter_handle, user_question):
