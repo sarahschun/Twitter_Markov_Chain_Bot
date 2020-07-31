104f91b4f004868ccf263b8199e7a2a7e0a4984b
Glitch (basic-training-ii) <none>
2020-07-31 15:41:48 +0000

    💅💝 Checkpoint ./markov.py:20186336/403

--- markov.py
+++ markov.py
@@ -12,7 +12,18 @@ def generate_bot_answer_with_text_model(twitter_handle, user_question, text_mode
 
     word_list = user_question.split("")
     random_word = random.choice(word_list)
-# build the markov chain based on the text we read
+    bot_answer = text_model.make_sentence_with_start(user_question, strict = False)
+print("Print")
+    if bot_answer = None
+    bot_answer = tex_model.make_sentence()
+    
+    if bot_answer = None
+    bot_answer =  generate_bot_answer_with_text_model(twitter_handle,
+user_question, text_model)
+    
+    return bot_answer
+
+#build the markov chain based on the text we read
 # we use the markovify library to do this step
 def generate_bot_answer(twitter_handle, user_question):
     pass
